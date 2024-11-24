b35c5a904d96a292e1c679c7454d658c8f73fc02
nicomi99 <mirabellinicolas@gmail.com>
2024-11-24 23:02:03 +0100

    modified:   src/ud4funciones/Ejercicio42.java

--- src/ud4funciones/Ejercicio42.java
+++ src/ud4funciones/Ejercicio42.java
@@ -6,7 +6,7 @@ public class Ejercicio42 {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (i == 1 && j == 1) {
-					System.out.print(" ");
+					System.out.print("Nicolas Mirabelli Boimorto");
 				} else {
 					System.out.print("*");
 				}
