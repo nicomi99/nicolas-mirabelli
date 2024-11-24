60cfe8e1c109149a762c1aa2aad052976874d725
nicomi99 <mirabellinicolas@gmail.com>
2024-11-24 22:12:35 +0100

    primera version de ficheros

---
+++ src/ud4funciones/Ejercicio43.java
@@ -0,0 +1,21 @@
+package ud4funciones;
+
+public class Ejercicio43 {
+	static void dibujarRectangulo(int ancho, int alto) {
+
+		if (ancho <= 0 || alto <= 0) { 
+			System.out.println("Error: El ancho y el alto del rectángulo deben ser positivos."); 
+			return; 
+		}
+			for (int i = 0; i < alto; i++) {
+			for (int j = 0; j < ancho; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		dibujarRectangulo(-5, 8);
+	}
+}
